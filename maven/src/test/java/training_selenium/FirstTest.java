64ab39a7946b7ac6af839a8c24441f3caef1f570
Belaya Vera <vera.belaya2018@yandex.ru>
2021-11-22 18:55:38 +0300

    Cценарий для входа в панель администрирования localhost

--- maven/src/test/java/training_selenium/FirstTest.java
+++ maven/src/test/java/training_selenium/FirstTest.java
@@ -3,6 +3,7 @@ package training_selenium;
 
 import org.junit.After;
 import org.junit.Before;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -18,13 +19,16 @@ public class FirstTest {
     @Before
     public void start() {
         driver = new ChromeDriver();
-        wait = new WebDriverWait(driver, 10);
+        wait = new WebDriverWait (driver, 20);
     }
 
 
     @Test
     public void FirstTest() {
-        driver.get("http://www.google.com");
+        driver.get("http://localhost/litecart/admin/");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
     }
 
     @After
